ea35e1c769dd09a50c9ad31c33326e9219cc2005
CSR-24 <chetan.raikwal@yahoo.com>
2017-08-27 13:48:07 +0530

    Add src folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import _ from 'lodash';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  userName = '';
+  isUserNameSet(event:any){
+   return _.isEmpty(this.userName) ? true : false;
+  }
+  clearUserName(){
+    this.userName = '';
+  }
 }
